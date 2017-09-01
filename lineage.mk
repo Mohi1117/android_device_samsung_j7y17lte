677a341691140ce0694e6670c89c5e60af5277f2
yukick1117 <mohiuddinkafre@gmail.com>
2017-09-01 07:19:19 +0000

    j7y17lte:initial lineage bringup -derped asf -still need colloborators

---
+++ lineage.mk
@@ -0,0 +1,15 @@
+#Inherit device configuration
+$(call inherit-product, device/samsung/j7y17lte/device.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := j7y17lte
+PRODUCT_NAME := lineage_j7y17lte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-J730GM
---
+++ vendorsetup.sh
@@ -0,0 +1,2 @@
+add_lunch_combo lineage_j7y17lte-userdebug
+add_lunch_combo lineage_j7y17lte-eng
