fb8bb3d44101508bc30235534a07f69166d2c602
yukick1117 <mohiuddinkafre@gmail.com>
2017-09-01 10:50:00 +0000

    remove dthbtool

--- device.mk
+++ device.mk
@@ -6,8 +6,8 @@ LOCAL_PATH := device/samsung/j7y17lte
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 #custom dtb
-PRODUCT_PACKAGES += \
-    dtbhtoolExynos
+#PRODUCT_PACKAGES += \
+#    dtbhtoolExynos
 
 #offmode charger
 PRODUCT_PACKAGES += \
