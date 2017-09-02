175e117a2df2a8be40169ac649cf91add42b8e17
yukick1117 <mohiuddinkafre@gmail.com>
2017-09-02 15:37:48 +0000

    j7y17lte:split up kernel flags

---
+++ board/kernel.mk
@@ -0,0 +1,15 @@
+# kernel
+#BOARD_KERNEL_CMDLINE := 
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+#BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --boa$
+#TARGET_PREBUILT_KERNEL := device/samsung/j7y17lte/kernel
+#TARGET_PREBUILT_DTB := device/samsung/j7y17lte/dt.img
+TARGET_KERNEL_CONFIG := j7y17lte_eur_openm_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/j7y17lte
+#TARGET_USES_UNCOMPRESSED_KERNEL := true
+#BOARD_KERNEL_SEPERATED_DT := true
+#TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
