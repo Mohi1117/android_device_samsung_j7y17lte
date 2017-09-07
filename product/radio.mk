a3752ac9500601672b36dbb0d8c294602ceedb59
yukick1117 <mohiuddinkafre@gmail.com>
2017-09-07 10:58:10 +0000

    j7y17lte:radio:split up to new device configuration

---
+++ product/radio.mk
@@ -0,0 +1,31 @@
+# Init Resources
+PRODUCT_PACKAGES += \
+	init.baseband.rc \
+	init.baseband.sh \
+	init.rilchip.rc \
+	init.rilcommon.rc
+
+# Packages
+PRODUCT_PACKAGES += \
+	libxml2 \
+	libprotobuf-cpp-full \
+	libprotobuf-cpp-lite \
+	libsecril-client
+
+# Permissions
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES +\= \
+	persist.radio.add_power_save=1 \
+	persist.radio.apm_sim_not_pwdn=1 \
+	rild.libpath=/system/lib/libsec-ril.so \
+	rild.libpath2=/system/lib/libsec-ril-dsds.so \
+	ro.telephony.default_network=9 \
+	ro.telephony.ril_class=SlteRIL \
+	ro.ril.gprsclass=10 \
+	ro.ril.hsxpa=1 \
+	ro.ril.telephony.mqanelements=6 \
+	telephony.lteOnGsmDevice=1 \
+	telephony.lteOnCdmaDevice=0
