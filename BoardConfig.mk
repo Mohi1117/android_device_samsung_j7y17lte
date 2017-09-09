af5036a0c8011e6d17972e820876565f811284b1
yukick1117 <mohiuddinkafre@gmail.com>
2017-09-09 08:59:40 +0000

    j7y17lte:improvise-1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,20 @@
+LOCAL_PATH := device/samsung/j7y17lte
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# SELinux
+BOARD_SEPOLICY_DIRS := device/samsung/j7y17lte/sepolicy
+
+# SECComp filters
+BOARD_SECCOMP_POLICY += device/samsung/j7y17lte/seccomp
+
+# Inherit board specific defines
+-include device/samsung/j7y17lte/board/*.mk
+
+# Inherit from the proprietary version
+-include vendor/samsung/j7y17lte/BoardConfigVendor.mk
+
 USE_CAMERA_STUB := true
 
 TARGET_NO_BOOTLOADER := true
@@ -17,36 +34,36 @@ TARGET_BOARD_PLATFORM_GPU := mali-t830mp2
 #################################HALS
 #gatekeeper
 PRODUCT_PACKAGES += \
-gatekeeper.exynos7870
+	gatekeeper.exynos7870
 
 #hwcomposer
 PRODUCT_PACKAGES += \
-hwcomposer.exynos5
+	hwcomposer.exynos5
 
 #keystore
 PRODUCT_PACKAGES += \
-keystore.default \
-keystore.exynos7870
+	keystore.default \
+	keystore.exynos7870
 
 #lights
 PRODUCT_PACKAGES += \
-lights.goldfish
+	lights.goldfish
 
 #time
 PRODUCT_PACKAGES += \
-local_time.default
+	local_time.default
 
 #memtrack
 PRODUCT_PACKAGES += \
-memtrack.exynos5.so
+	memtrack.exynos5.so
 
 #power
 PRODUCT_PACKAGES += \
-power.default \
-power.exynos5 \
-power.goldfish
+	power.default \
+	power.exynos5 \
+	power.goldfish
 
 #vibrator
 PRODUCT_PACKAGES += \
-vibrator.default \
-vibrator.goldfish
+	vibrator.default \
+	vibrator.goldfish
