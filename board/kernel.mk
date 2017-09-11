df9e6eaac7665255f4db9605f577a97e2dfd6f33
yukick1117 <mohiuddinkafre@gmail.com>
2017-09-11 10:50:44 +0000

    j7y17lte:board:kernel:Set selinux to permissive

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,5 @@
 # kernel
-BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
