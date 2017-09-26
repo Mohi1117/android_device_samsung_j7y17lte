023cd1f1ea4e56dc5bfad4379f15e9a2010fef5b
yukick1117 <mohiuddinkafre@gmail.com>
2017-09-26 13:44:51 +0000

    j7y17lte:misc add/removal

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,7 @@
 # kernel
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPQA18A000KU --dt $(LOCAL_PATH)/dt.img
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPQA18A000KU
 TARGET_KERNEL_CONFIG := j7y17ltem_00_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/j7y17lte
 TARGET_KERNEL_ARCH := arm64
@@ -9,6 +9,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 BOARD_KERNEL_SEPERATED_DT := true
 #TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
+TARGET_KERNEL_ARCH := arm64
 ifeq ($(FORCE_32_BIT),true)
 TARGET_KERNEL_HEADER_ARCH := arm
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
--- product/media.mk
+++ product/media.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
-        frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+        frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	external/stagefright-plugins/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
 	$(DEVICE_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
 	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
